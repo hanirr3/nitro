d20c458954f07a69d4e20559b1438979f899033f
hanirr3 <44904038+hanirr3@users.noreply.github.com>
2018-11-30 10:47:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
 
 client.on('ready', async() => {
 var server = "517965562849263619"; // ايدي السررفر
-var channel = "517965562849263619";//ايدي الروم
+var channel = "517968522513743872";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
     },305);
